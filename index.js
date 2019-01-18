3ab9ab1d8af00ef76f89c78a55a570bbf4a9989b
github username <github email address>
2019-01-17 19:59:42 -0500

    implemented passport and received accessToken from Google

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile','email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+// passport will see 'code' in the URL and know this is not the first
+// attempt to autheticate. It iwll try to exchange the code for a user profile
+
+
 
 
 

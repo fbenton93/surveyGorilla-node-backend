15de27f8c51f68946775d6f6a4a8a1eab242f2c1
github username <github email address>
2019-01-18 20:09:54 -0500

    implemented logut path

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,13 @@ module.exports = (app) => {
   // passport will see 'code' in the URL and know this is not the first
   // attempt to autheticate. It wll try to exchange the code for a user profile
 
+  app.get('/api/logout', (req,res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+
+  app.get('/api/currentUser', (req,res) => {
+    res.send(req.user);
+  });
 };

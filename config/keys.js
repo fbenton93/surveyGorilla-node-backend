e6f300ac7ac214bbb91f32cc464d3a633e5c2186
github username <github email address>
2019-01-19 11:38:06 -0500

    pushing up prod/dev key configuration

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 // LOGIC TO RETURN CORRECT KEYS FOR EITHER PROD OR DEV
 if(process.env.NODE_ENV === 'production') {
-  // exports prod keys
   module.exports = require('./prod');
 } else {
-  // exports dev keys
   module.exports = require('./dev');
 }

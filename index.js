387459db09f4a183638581d2054316f36c9ce9c3
github username <github email address>
2019-01-21 14:20:05 -0500

    finalized for redeployment

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+// PRODUCTION-ONLY ROUTING CONFIGURATION
+if(process.env.NODE_ENV === 'production') {
+  // serving productions assets
+  app.use(express.static('client/build'));
+  // serving up index.html file if route is unrecognized
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 
 // SETTING EXPRESS ON PORT
 const PORT = process.env.PORT || 5000;
